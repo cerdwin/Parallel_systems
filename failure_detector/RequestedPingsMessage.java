2513c872148632a227bcef291f4ec4198579b45c
cerdwin <47043669+cerdwin@users.noreply.github.com>
2021-01-19 17:58:30 +0100

    Create RequestedPingsMessage.java

---
+++ failure_detector/RequestedPingsMessage.java
@@ -0,0 +1,12 @@
+package cz.cvut.fel.agents.pdv.swim;
+ 
+import cz.cvut.fel.agents.pdv.dsand.Message;
+ 
+public class RequestedPingsMessage extends Message {
+ 
+ 
+    /*
+    public String toString() {
+        return "PING " + this.processID;
+    }*/
+}
